9f17e8ebfe93831eaf87f986fad257352c2e9d48
haneesh1234-ai <knsshaneesh@gmail.com>
2026-03-28 17:55:45 +0530

    Add files via upload

---
+++ Data types in python/data type.py
@@ -0,0 +1,8 @@
+name= "haneesh" 
+age = 10
+weigth = 35.5
+student = True
+print(type(name))
+print(type(age))
+print(type(weigth))
+print(type(student)
---
+++ Data types in python/type casting.py
@@ -0,0 +1,9 @@
+# integer to float
+age = 10 
+age= float(age)
+print(type(age),age)
+
+#float to integer
+weigth = 35.5
+weigth = int(weigth)
+print(type(weigth),weigth)
